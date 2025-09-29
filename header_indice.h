43b2f09df3b42f76341c92553b3aac8de6a45199
diegomoriR <diegomorirodrigues@usp.br>
2025-09-29 17:47:56 -0300

    Add files via upload

---
+++ header_indice.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_INDICE_H
+#define HEADER_INDICE_H
+
+typedef struct{
+    char status;
+    char lixo[11] = {$,$,$,$,$,$,$,$,$,$,$};
+}
+
+#endif
---
+++ header_pessoa.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_PESSOA_H
+#define HEADER_PESSOA_H
+
+typedef struct _header {
+    char status;
+    int quantidadePessoas;
+    int quantidadeRemovidos;
+    int Offset;
+}header;
+
+#endif
