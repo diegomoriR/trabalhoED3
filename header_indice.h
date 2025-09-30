f07ee3df7f8a44746adf402f6fb7a4cbee5e1e3d
diegomoriR <diegomorirodrigues@usp.br>
2025-09-30 15:37:34 -0300

    header_indice.h

--- header_indice.h
+++ header_indice.h
@@ -1,9 +1,14 @@
 #ifndef HEADER_INDICE_H
 #define HEADER_INDICE_H
 
-typedef struct{
+typedef struct _headerI{
     char status;
-    char lixo[11] = {$,$,$,$,$,$,$,$,$,$,$};
-}
+    char lixo[11];
+}headerI;
+
+typedef struct _indice{
+    int idPessoa;
+    long offSet;
+}indice;
 
 #endif
