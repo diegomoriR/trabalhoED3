8d2946756c3975fa708ee241e1ad516504800dc8
diegomoriR <diegomorirodrigues@usp.br>
2025-09-30 15:36:41 -0300

    header_pessoa.h

--- header_pessoa.h
+++ header_pessoa.h
@@ -5,7 +5,22 @@ typedef struct _header {
     char status;
     int quantidadePessoas;
     int quantidadeRemovidos;
-    int Offset;
+    long Offset;
 }header;
 
+typedef struct _registroPessoa{
+    char removido;
+    int tamanhoRegistro;
+    int idPessoa;
+    int idadePessoa;
+    int tamanhoNomePessoa;
+    char nomePessoa[];
+    int tamanhoNomeUsuario;
+    char nomeUsuario[];
+}pessoa;
+
+#define VERIFICAR_ARQUIVO(fd) (if(fd == NULL){printf("Falha no processamento do arquivo.\n");})
+#define TAMANHO_INDICE 12
+#define INICIO_ARQUIVO(fd) (fseek(fd, 0 , SEEK_SET);)
+#define TAMANHO_LINHA 1024
 #endif
