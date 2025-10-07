e0977b1bec4c37942c6cda976b842dde9146eaa8
diegomoriR <diegomorirodrigues@usp.br>
2025-10-07 18:30:50 -0300

    Update funcionalidade1.c

--- funcionalidade1.c
+++ funcionalidade1.c
@@ -4,18 +4,17 @@
 #include "header_pessoa.h"
 #include "utilidades.h"
 
-//funcionalidade 1 (CREATE INDEX)
 void CREATE_INDEX(char *arquivoIndicePrimario){
     FILE *fd = fopen(arquivoIndicePrimario, "wb"); //abrindo o arquivo para a escrita binaria
-    VERIFICAR_ARQUIVO(fd); // verificando se o arquivo foi aberto corretamente
+    VERIFICAR_ARQUIVO(fd) // verificando se o arquivo foi aberto corretamente
     headerIndice h;
     h.status = '0'; // status inconsistente
     fwrite(&h.status, sizeof(char), 1, fd);
-    for(int i = 0; i < TAMANHO_INDICE - 1, i++){
+    for(int i = 0; i < TAMANHO_INDICE - 1; i++){
         fputc('$', fd);
     }
     h.status = '1'; //status consistente
-    INICIO_ARQUIVO(fd) // cursor do arquivo para o inicio
+    INICIO_ARQUIVO(fd); // cursor do arquivo para o inicio
     fwrite(&h.status, sizeof(char), 1, fd);
     fclose(fd); //fechando arquivo
     binarioNaTela(arquivoIndicePrimario);
