be36c99dbe7d893ad347defd76f45a822cce82e7
diegomoriR <diegomorirodrigues@usp.br>
2025-10-07 18:41:20 -0300

    Update funcionalidade1.c

--- funcionalidade1.c
+++ funcionalidade1.c
@@ -6,7 +6,9 @@
 
 void CREATE_INDEX(char *arquivoIndicePrimario){
     FILE *fd = fopen(arquivoIndicePrimario, "wb"); //abrindo o arquivo para a escrita binaria
-    VERIFICAR_ARQUIVO(fd) // verificando se o arquivo foi aberto corretamente
+    if(fd == NULL){
+        printf("Falha no processamento do arquivo.\n");// verificando se o arquivo foi aberto corretamente
+    } 
     headerIndice h;
     h.status = '0'; // status inconsistente
     fwrite(&h.status, sizeof(char), 1, fd);
