cfacc392a3f55720f6cf16fae3f72bfa54ad5107
lucas <lsantos.stu@gmail.com>
2025-10-27 19:01:14 -0300

    func7

---
+++ funcionalidade7.c
@@ -0,0 +1,224 @@
+//Lucas Soares Leite Santos - 15472162
+//Diego Mori Rodrigues - 13782421
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header_pessoa.h"
+
+
+//funcionalidade 7 UPDATE
+void UPDATE(char* arquivoEntrada, char* arquivoIndice, int n){
+
+
+    FILE *fdin = fopen(arquivoEntrada, "rb"); // abrindo o arquivo para ler os dados
+    if(fdin == NULL){
+        printf("Falha no processamento do arquivo.\n");// verificando se o arquivo foi aberto corretamente
+        return;
+    }
+
+    FILE *fdh = fopen(arquivoIndice, "rb"); // abrindo o arquivo para a escrita binaria no indice
+    if(fdh == NULL){
+        printf("Falha no processamento do arquivo.\n");// verificando se o arquivo foi aberto corretamente
+        return;
+    }
+    int buscas[n];
+    int i, param, k, Ncampo;
+    char tipoBusca[15], campo[15];
+    char parametro[30];
+    char NcampoString[30];
+
+    // criando headers e pessoas pra facilitar a manipulação de dados
+    headerIndice hi;
+    header h;
+    pessoa p;
+    indice j;
+    
+    for( i=0; i<n;i++){//controle para o número de buscas
+
+
+        scanf("%d",&buscas[i]);//numero da busca com o parametro
+        scanf(" %[^=]", tipoBusca);
+        scanf("%*c");
+        fseek(fdin, 0, SEEK_SET);//coloca no começo do arquivo pessoa
+
+        if(strcmp(tipoBusca,"idPessoa")==0){
+            
+            scanf("%d",&param);// pega o parametro a ser buscado
+            scanf("%*c");
+            scanf(" %[^=]", campo);
+            scanf("%*c");
+            fread(&hi.status,sizeof(char),1,fdh);// vê o status do arquivo de indice
+            fseek(fdh,12, SEEK_SET);// vai para os dados do arquivo indice
+            fread(&h.status,sizeof(char),1,fdin);//vê o status do arquivo pessoa
+            fread(&h.quantidadePessoas,sizeof(int),1,fdin);//vê o número de pessoas
+
+             for(k=0;k<h.quantidadePessoas;k++){//busca até o número total de pessoas
+                
+                
+             fread(&j.idPessoa,sizeof(int),1,fdh);//lê o id da pessoa no arquivo indice
+             if(j.idPessoa == param){//verifica se é igual ao parâmetro buscado
+             
+               fread(&j.Offset,sizeof(long),1,fdh);//lê o byte offset
+               fseek(fdin,j.Offset,SEEK_SET);  //vai para o offset no arquivo pessoa   
+               fread(&p.removido,sizeof(char), 1, fdin);//vê o caracter 0 ou 1 de remoção
+               fread(&p.tamanhoRegistro, sizeof(int), 1, fdin);//pega o tamanho do registro
+               if(p.removido == '0'){ //registro nao esta marcado como removido
+
+                //pega os dados da pessoa para imprimir
+                 fread(&p.idPessoa, sizeof(int), 1, fdin);
+                 fread(&p.idadePessoa, sizeof(int), 1, fdin);
+                 fread(&p.tamanhoNomePessoa, sizeof(int), 1, fdin);
+                 p.nomePessoa = (char *) malloc(p.tamanhoNomePessoa + 1);
+                 fread(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fdin);
+                 p.nomePessoa[p.tamanhoNomePessoa] = 0;
+                 fread(&p.tamanhoNomeUsuario,sizeof(int), 1, fdin);
+                 p.nomeUsuario = (char *) malloc(p.tamanhoNomeUsuario + 1);
+                 fread(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fdin);
+                 p.nomeUsuario[p.tamanhoNomeUsuario] = 0;
+
+                substitui_registro(fdin, p,campo);
+                free(p.nomePessoa);
+                free(p.nomeUsuario);               
+               }
+            }else{fseek(fdh,sizeof(long),SEEK_CUR);}//vai para o próximo id no arquivo indice
+            }
+           
+        }// caso busca por id
+
+
+        else if(strcmp(tipoBusca,"idadePessoa")==0){
+            scanf("%d",&param);// pega o parametro a ser buscado
+            scanf("%*c");
+            scanf(" %[^=]", campo);
+            scanf("%*c");
+            fseek(fdin, 1, SEEK_SET); // cursor para o inicio do registro de cabecalho
+            fread(&h.quantidadePessoas, sizeof(int), 1, fdin);
+            fread(&h.quantidadeRemovidos,sizeof(int), 1, fdin);
+            fread(&h.Offset,sizeof(long), 1, fdin);
+            for(k=0;k<h.quantidadePessoas;k++){
+
+        fread(&p.removido,sizeof(char), 1, fdin);
+        fread(&p.tamanhoRegistro, sizeof(int), 1, fdin);
+        
+        if(p.removido == '0'){ //registro nao esta marcado como removido
+            
+            fread(&p.idPessoa, sizeof(int), 1, fdin);
+            fread(&p.idadePessoa, sizeof(int), 1, fdin);
+            fread(&p.tamanhoNomePessoa, sizeof(int), 1, fdin);
+            p.nomePessoa = (char *) malloc(p.tamanhoNomePessoa + 1);
+            fread(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fdin);
+            p.nomePessoa[p.tamanhoNomePessoa] = 0;
+            fread(&p.tamanhoNomeUsuario,sizeof(int), 1, fdin);
+            p.nomeUsuario = (char *) malloc(p.tamanhoNomeUsuario+1);
+            fread(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fdin);
+            p.nomeUsuario[p.tamanhoNomeUsuario] = 0;
+                 
+                 if(p.idadePessoa == param){//verifica se é o parâmetro buscado
+                    
+                substitui_registro(fdin, p,campo);
+                free(p.nomePessoa);
+                free(p.nomeUsuario);
+
+            }
+            }else{fseek(fdin,p.tamanhoRegistro,SEEK_CUR);//registro removido, vai para o próximo
+                free(p.nomePessoa);
+                free(p.nomeUsuario);}
+            }
+            
+        }//caso busca idade
+
+
+
+        else if(strcmp(tipoBusca,"nomePessoa")==0){
+            scan_quote_string(parametro);
+            scanf("%*c");
+            scanf(" %[^=]", campo);
+            scanf("%*c");
+            fseek(fdin, 1, SEEK_SET); // cursor para o inicio do registro de cabecalho
+            fread(&h.quantidadePessoas, sizeof(int), 1, fdin);
+            fread(&h.quantidadeRemovidos,sizeof(int), 1, fdin);
+            fread(&h.Offset,sizeof(long), 1, fdin);
+            for(k=0;k<h.quantidadePessoas;k++){
+
+        fread(&p.removido,sizeof(char), 1, fdin);
+        fread(&p.tamanhoRegistro, sizeof(int), 1, fdin);
+        
+        if(p.removido == '0'){ //registro nao esta marcado como removido
+            
+            fread(&p.idPessoa, sizeof(int), 1, fdin);
+            fread(&p.idadePessoa, sizeof(int), 1, fdin);
+            fread(&p.tamanhoNomePessoa, sizeof(int), 1, fdin);
+            p.nomePessoa = (char *) malloc(p.tamanhoNomePessoa + 1);
+            fread(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fdin);
+            p.nomePessoa[p.tamanhoNomePessoa] = 0;
+            fread(&p.tamanhoNomeUsuario,sizeof(int), 1, fdin);
+            p.nomeUsuario = (char *) malloc(p.tamanhoNomeUsuario+1);
+            fread(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fdin);
+            p.nomeUsuario[p.tamanhoNomeUsuario] = 0;
+                 
+                 if(strcmp(parametro,p.nomePessoa)==0){//verifica se é o parâmetro buscado
+                    
+                substitui_registro(fdin, p,campo);
+                free(p.nomePessoa);
+                free(p.nomeUsuario);
+
+            }
+            }else{fseek(fdin,p.tamanhoRegistro,SEEK_CUR);//registro removido, vai para o próximo
+                free(p.nomePessoa);
+                free(p.nomeUsuario);}
+            }
+            
+        }//caso busca nome pessoa
+
+
+        else if(strcmp(tipoBusca,"nomeUsuario")==0){
+            scan_quote_string(parametro);
+            scanf("%*c");
+            scanf(" %[^=]", campo);
+            scanf("%*c");
+            fseek(fdin, 1, SEEK_SET); // cursor para o inicio do registro de cabecalho
+            fread(&h.quantidadePessoas, sizeof(int), 1, fdin);
+            fread(&h.quantidadeRemovidos,sizeof(int), 1, fdin);
+            fread(&h.Offset,sizeof(long), 1, fdin);
+            for(k=0;k<h.quantidadePessoas;k++){
+
+        fread(&p.removido,sizeof(char), 1, fdin);
+        fread(&p.tamanhoRegistro, sizeof(int), 1, fdin);
+        
+        if(p.removido == '0'){ //registro nao esta marcado como removido
+            
+            fread(&p.idPessoa, sizeof(int), 1, fdin);
+            fread(&p.idadePessoa, sizeof(int), 1, fdin);
+            fread(&p.tamanhoNomePessoa, sizeof(int), 1, fdin);
+            p.nomePessoa = (char *) malloc(p.tamanhoNomePessoa + 1);
+            fread(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fdin);
+            p.nomePessoa[p.tamanhoNomePessoa] = 0;
+            fread(&p.tamanhoNomeUsuario,sizeof(int), 1, fdin);
+            p.nomeUsuario = (char *) malloc(p.tamanhoNomeUsuario+1);
+            fread(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fdin);
+            p.nomeUsuario[p.tamanhoNomeUsuario] = 0;
+                 
+                 if(strcmp(parametro,p.nomeUsuario)==0){//verifica se é o parâmetro buscado
+                    
+                substitui_registro(fdin, p,campo);
+                free(p.nomePessoa);
+                free(p.nomeUsuario);
+
+            }
+            }else{fseek(fdin,p.tamanhoRegistro,SEEK_CUR);//registro removido, vai para o próximo
+                free(p.nomePessoa);
+                free(p.nomeUsuario);}
+            }
+           
+        }//caso busca nome Usuário
+     
+
+    }
+
+    fclose(fdin);
+    fclose(fdh);
+    return;
+
+}
---
+++ utilidades.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "header_pessoa.h"
+
+/*
+Função para imprimir dados salvos no arquivo em binário
+(util para comparar saida no run codes)
+*/
+void binarioNaTela(char *nomeArquivoBinario) { 
+
+	/* Use essa função para comparação no run.codes. Lembre-se de ter fechado (fclose) o arquivo anteriormente.
+	*  Ela vai abrir de novo para leitura e depois fechar (você não vai perder pontos por isso se usar ela). */
+
+	unsigned long i, cs;
+	unsigned char *mb;
+	size_t fl;
+	FILE *fs;
+	if(nomeArquivoBinario == NULL || !(fs = fopen(nomeArquivoBinario, "rb"))) {
+		fprintf(stderr, "ERRO AO ESCREVER O BINARIO NA TELA (função binarioNaTela): não foi possível abrir o arquivo que me passou para leitura. Ele existe e você tá passando o nome certo? Você lembrou de fechar ele com fclose depois de usar?\n");
+		return;
+	}
+	fseek(fs, 0, SEEK_END);
+	fl = ftell(fs);
+	fseek(fs, 0, SEEK_SET);
+	mb = (unsigned char *) malloc(fl);
+	fread(mb, 1, fl, fs);
+
+	cs = 0;
+	for(i = 0; i < fl; i++) {
+		cs += (unsigned long) mb[i];
+	}
+	printf("%lf\n", (cs / (double) 100));
+	free(mb);
+	fclose(fs);
+}
+
+void scan_quote_string(char *str) {
+
+	/*
+	*	Use essa função para ler um campo string delimitado entre aspas (").
+	*	Chame ela na hora que for ler tal campo. Por exemplo:
+	*
+	*	A entrada está da seguinte forma:
+	*		nomeDoCampo "MARIA DA SILVA"
+	*
+	*	Para ler isso para as strings já alocadas str1 e str2 do seu programa, você faz:
+	*		scanf("%s", str1); // Vai salvar nomeDoCampo em str1
+	*		scan_quote_string(str2); // Vai salvar MARIA DA SILVA em str2 (sem as aspas)
+	*
+	*/
+
+	char R;
+
+	while((R = getchar()) != EOF && isspace(R)); // ignorar espaços, \r, \n...
+
+	if(R == 'N' || R == 'n') { // campo NULO
+		getchar(); getchar(); getchar(); // ignorar o "ULO" de NULO.
+		strcpy(str, ""); // copia string vazia
+	} else if(R == '\"') {
+		if(scanf("%[^\"]", str) != 1) { // ler até o fechamento das aspas
+			strcpy(str, "");
+		}
+		getchar(); // ignorar aspas fechando
+	} else if(R != EOF){ // vc tá tentando ler uma string que não tá entre aspas! Fazer leitura normal %s então, pois deve ser algum inteiro ou algo assim...
+		str[0] = R;
+		scanf("%s", &str[1]);
+	} else { // EOF
+		strcpy(str, "");
+	}
+}
+/* ---------------- EXTRA ----------------
+
+OPCIONAL: dicas sobre scanf() e fscanf():
+
+scanf("%[^,]", string) -> lê até encontrar o caractere ',', não incluindo o mesmo na leitura.
+
+Exemplo de entrada: "Oi, esse é um exemplo."
+Nesse caso, o scanf("%[^,]") tem como resultado a string "Oi";
+
+scanf("%[^\"]", string) -> lê até encontrar o caractere '"', não incluindo o mesmo na leitura.
+scanf("%[^\n]", string) -> lê até encontrar o fim da linha, não incluindo o '\n' na leitura.
+
+scanf("%*c") --> lê um char e não guarda em nenhuma variável, como se tivesse ignorado ele
+
+*/
+
+
+char* strsep(char** stringp, const char* delim) {
+    if (stringp == NULL || *stringp == NULL) {
+        return NULL;
+    }
+    
+    char* start = *stringp;
+    char* p = strpbrk(start, delim); // Encontra o primeiro delimitador
+
+    if (p == NULL) {
+        // Nenhum delimitador encontrado, este é o último token
+        *stringp = NULL;
+    } else {
+        // Delimitador encontrado
+        *p = '\0';             // Substitui o delimitador por nulo
+        *stringp = p + 1;      // Avança o ponteiro principal para depois do nulo
+    }
+
+    return start;
+}
+
+
+void print_registro(pessoa p){
+
+            //idPessoa
+            if(p.idPessoa != -1){
+                printf("Dados da pessoa de codigo %d\n", p.idPessoa);
+            }else{
+                printf("Dados da pessoa de codigo -\n");
+            }
+            //nomePessoa
+            if(p.tamanhoNomePessoa != 0){
+                printf("Nome: %s\n", p.nomePessoa);
+            }else{
+                printf("Nome: - \n");
+            }
+            //idadePessoa
+            if(p.idadePessoa != -1){
+                printf("Idade: %d\n", p.idadePessoa);
+            }else{
+                printf("Idade: -\n");
+            }
+            //usuarioPessoa
+            if(p.tamanhoNomeUsuario != 0){
+                printf("Usuario: %s\n", p.nomeUsuario);
+            }else{
+                printf("Usuario: - \n");
+            }
+            printf("\n");
+
+}
+
+void substitui_registro(FILE* fd,  pessoa p,char campo){
+
+ int Ncampo;
+ char NcampoString[30];
+ char lixo = '$';
+
+	fseek(fd, -p.tamanhoRegistro, SEEK_CUR);
+
+	if(strcmp(campo,"nomePessoa")==0){
+	
+		scan_quote_string(NcampoString);
+
+		if(p.tamanhoNomePessoa>=strlen(NcampoString)){
+			fseek(fd,8,SEEK_CUR);
+			fwrite(strlen(NcampoString),sizeof(int),1,fd);
+			fwrite(NcampoString,sizeof(char),strlen(NcampoString),fd);
+			fwrite(lixo,sizeof(char),p.tamanhoNomePessoa-strlen(Ncampo),fd);
+		}
+
+		else{
+			p.tamanhoNomePessoa = strlen(NcampoString);
+			strcpy(p.nomePessoa,NcampoString);
+			p.tamanhoRegistro = 16 + p.tamanhoNomePessoa + p.tamanhoNomeUsuario;
+			fseek(fd,0,SEEK_END);
+			fwrite(&p.removido, sizeof(char), 1, fd);
+        	fwrite(&p.tamanhoRegistro, sizeof(int), 1, fd);
+        	fwrite(&p.idPessoa, sizeof(int), 1, fd);
+        	fwrite(&p.idadePessoa, sizeof(int), 1, fd);
+        	fwrite(&p.tamanhoNomePessoa, sizeof(int), 1, fd);
+        	fwrite(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fd);
+        	fwrite(&p.tamanhoNomeUsuario, sizeof(int), 1, fd);
+			fwrite(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fd);
+			}
+	}
+
+
+	else if(strcmp(campo,"nomeUsuario")==0){
+		scan_quote_string(NcampoString);
+
+		if(p.tamanhoNomePessoa>=strlen(NcampoString)){
+			fseek(fd,12+p.tamanhoNomePessoa,SEEK_CUR);
+			fwrite(strlen(NcampoString),sizeof(int),1,fd);
+			fwrite(NcampoString,sizeof(char),strlen(NcampoString),fd);
+			fwrite(lixo,sizeof(char),p.tamanhoNomePessoa-strlen(Ncampo),fd);
+		}
+
+		else{
+			p.tamanhoNomeUsuario = strlen(NcampoString);
+			strcpy(p.nomeUsuario,NcampoString);
+			p.tamanhoRegistro = 16 + p.tamanhoNomePessoa + p.tamanhoNomeUsuario;
+			fseek(fd,0,SEEK_END);
+			fwrite(&p.removido, sizeof(char), 1, fd);
+        	fwrite(&p.tamanhoRegistro, sizeof(int), 1, fd);
+        	fwrite(&p.idPessoa, sizeof(int), 1, fd);
+        	fwrite(&p.idadePessoa, sizeof(int), 1, fd);
+        	fwrite(&p.tamanhoNomePessoa, sizeof(int), 1, fd);
+        	fwrite(p.nomePessoa, sizeof(char), p.tamanhoNomePessoa, fd);
+        	fwrite(&p.tamanhoNomeUsuario, sizeof(int), 1, fd);
+			fwrite(p.nomeUsuario, sizeof(char), p.tamanhoNomeUsuario, fd);
+			}
+	}
+	else if(strcmp(campo,"idPessoa")==0){
+		scanf("%d",&Ncampo);
+
+			fwrite(&Ncampo,sizeof(int),1,fd);
+		
+	}
+	else if(strcmp(campo,"idadePessoa")==0){
+
+				scanf("%d",&Ncampo);
+			fseek(fd,4,SEEK_CUR);
+			fwrite(&Ncampo,sizeof(int),1,fd);
+	}
+
+}
