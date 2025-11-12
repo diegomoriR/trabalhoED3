f36da97e6e5f3654f8504c49132509d07657ce52
diegomoriR <diegomorirodrigues@usp.br>
2025-11-12 15:56:50 -0300

    Update funcionalidade5-2.c

--- funcionalidade5-2.c
+++ funcionalidade5-2.c
@@ -10,11 +10,13 @@ void DELETE(char *arquivoEntrada, char *arquivoIndicePrimario, int n){
     //abrindo os arquivos para leitura e escrita binaria
     FILE *fd = fopen(arquivoEntrada, "rb+");
     if(fd == NULL){
-        printf("Falha no processamento do arquivo\n");
+        printf("Falha no processamento do arquivo.\n");
+        return;
     }
     FILE *fdh = fopen(arquivoIndicePrimario, "rb+");
     if(fdh == NULL){
-        printf("Falha no processamento do arquivo\n");
+        printf("Falha no processamento do arquivo.\n");
+        return;
     }
 
     int buscas[n];
@@ -23,7 +25,8 @@ void DELETE(char *arquivoEntrada, char *arquivoIndicePrimario, int n){
     headerIndice hi;
     header h;
     pessoa *p;
-    indice *i;
+    indice i;
+
 
     //numero de buscas
     for(int j = 0; j < n; j++){
@@ -31,61 +34,78 @@ void DELETE(char *arquivoEntrada, char *arquivoIndicePrimario, int n){
         scanf(" %[^=]", tipoBusca);
         scanf("%*c");
         INICIO_ARQUIVO(fd); //coloca no começo do arquivo pessoa
-
-        //puxando o indice para a RAM
-        indice *indices = vetor_ind(fdh);
-        //lendo o indice para a RAM
+        //ler cabecalho
+        fread(&h.status, sizeof(char), 1, fd);
+        fread(&h.quantidadePessoas, sizeof(int), 1, fd);
+        fread(&h.quantidadeRemovidos, sizeof(int), 1, fd);
+        INICIO_ARQUIVO(fd); // coloca no comeco do arquivo pessoa
 
         //busca a pessoa p
         p = busca(fd, fdh, tipoBusca);
+
         if(p != NULL){
+            indice VetInd[NUMERO_PESSOAS];
+            fseek(fdh, 12, SEEK_SET);
+        //puxando o indice para a RAM
+            for(int in = 0; in < NUMERO_PESSOAS; in++){//copia os indices para o vetor
+                fread(&VetInd[in].idPessoa,sizeof(int),1,fdh);
+                fread(&VetInd[in].Offset,sizeof(long),1,fdh);
+            }
             //busca binaria no arquivo de indice para descobrir o offset para a remocao
-           
-
-            long Offset = busca_binaria_indice(indices, NUMERO_PESSOAS, p->idPessoa);
-
-
+            i.Offset = busca_binaria_indice(VetInd, NUMERO_PESSOAS, p->idPessoa);
+            if(i.Offset != -1){
+                long Offset = i.Offset;
             //remover no arquivo pessoa
-            fseek(fd, Offset, SEEK_SET);
-            p->removido = '1';
-            fwrite(&p->removido, sizeof(char), 1, fd);
+                fseek(fd, Offset, SEEK_SET);
+                p->removido = '1';
+                fwrite(&p->removido, sizeof(char), 1, fd);
 
             //shift para remover da RAM
-            int pos = i - indices;
-            for (int j = pos; j < NUMERO_PESSOAS -1; j++){
-                indices[j] = indices[j+1];
-            }
-            h.quantidadeRemovidos++;
+                int pos;
+                for(int in = 0; in < NUMERO_PESSOAS; in++){
+                    if(VetInd[in].Offset == i.Offset)
+                    pos = in;
+                }
+                for (int j = pos; j < NUMERO_PESSOAS -1; j++){
+                        VetInd[j] = VetInd[j+1];
+                }
+                h.quantidadeRemovidos++;
             //escrever indice no disco
             //status inconsistente
-            INICIO_ARQUIVO(fdh);
-            hi.status = '0';
-            fwrite(&hi.status, sizeof(char), 1, fdh);
+                INICIO_ARQUIVO(fdh);
+                hi.status = '0';
+                fwrite(&hi.status, sizeof(char), 1, fdh);
 
-            fseek(fdh, 12, SEEK_SET);
-            fwrite(indices, sizeof(indice), NUMERO_PESSOAS, fdh);
+                fseek(fdh, 12, SEEK_SET);
+                for(int in = 0; in < NUMERO_PESSOAS; in++){
+                    fwrite(&VetInd[in].idPessoa,sizeof(int),1,fdh);
+                    fwrite(&VetInd[in].Offset,sizeof(long),1,fdh);
+                }
 
             //status consistente
-            INICIO_ARQUIVO(fdh);
-            hi.status = '1';
-            fwrite(&hi.status, sizeof(char), 1, fdh);
+                INICIO_ARQUIVO(fdh);
+                hi.status = '1';
+                fwrite(&hi.status, sizeof(char), 1, fdh);
 
             //atualizar cabecalho
             //status inconsistente
-            INICIO_ARQUIVO(fd);
-            h.status = '0';
-            fwrite(&h.status, sizeof(char), 1, fd);
-             //campo quantidade removidos
-            fseek(fd, 5, SEEK_SET);
-            fwrite(&h.quantidadeRemovidos, sizeof(int), 1, fd);
-            //status consistente
-            INICIO_ARQUIVO(fdh);
-            hi.status = '1';
-            fwrite(&hi.status, sizeof(char), 1, fdh);
-
+                INICIO_ARQUIVO(fd);
+                h.status = '0';
+                fwrite(&h.status, sizeof(char), 1, fd);
+                //campo quantidade removidos
+                fseek(fd, 5, SEEK_SET);
+                fwrite(&h.quantidadeRemovidos, sizeof(int), 1, fd);
+                //status consistente
+                INICIO_ARQUIVO(fd);
+                h.status = '1';
+                fwrite(&h.status, sizeof(char), 1, fd);
+            }
 
         }else{
-            printf("Registro nao encontrado\n");
+            printf("Registro nao encontrado.\n");
         }
     }
+
+    binarioNaTela(arquivoEntrada);
+    binarioNaTela(arquivoIndicePrimario);
 }
