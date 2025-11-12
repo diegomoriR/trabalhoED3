79b56397e6a23a5d4aef964ee2da2521f20fd41f
diegomoriR <diegomorirodrigues@usp.br>
2025-11-12 15:56:24 -0300

    Update funcionalidade10.c

--- funcionalidade10.c
+++ funcionalidade10.c
@@ -5,29 +5,28 @@
 
 //funcionalidade 10 (JUNCAO)
 
-void JUNCAO(char *arquivoEntrada, char *arquivoIndicePrimario, char *arquivoOrdenado){
+void JUNCAO(char *arquivoEntrada, char *arquivoIndicePrimario, char *arquivoOrdenado, int n){
     FILE *fdin = fopen(arquivoEntrada, "rb");
     if(fdin == NULL){
-        printf("Falha no processamento do arquivo\n");
+        printf("Falha no processamento do arquivo.\n");
     }
     FILE *fdh = fopen(arquivoIndicePrimario, "rb");
     if(fdh == NULL){
-        printf("Falha no processamento do arquivo\n");
+        printf("Falha no processamento do arquivo.\n");
     }
     FILE *fdo = fopen(arquivoOrdenado, "rb");
     if(fdo == NULL){
-        printf("Falha no processamento do arquivo\n");
+        printf("Falha no processamento do arquivo.\n");
     }
 
     //inicializando para manipulacao dos dados
     header h;
     headerIndice hi;
     headerSegue hs;
-    pessoa *p;
+    pessoa *p = malloc(sizeof(pessoa));
     indice i;
     segue *s;
 
-    int n;
     int buscas[n];
     char tipoBusca[15];
 
@@ -37,13 +36,22 @@ void JUNCAO(char *arquivoEntrada, char *arquivoIndicePrimario, char *arquivoOrde
         scanf("%d",&buscas[j]);//numero da busca com o parametro
         scanf(" %[^=]", tipoBusca);
         scanf("%*c");
-        INICIO_ARQUIVO(fdh); //coloca no começo do arquivo
-        //puxando o indice para a RAM
-        indice *indices = (indice *)malloc(NUMERO_PESSOAS * sizeof(indice));
-        //lendo o indice para a RAM
-        fread(indices, sizeof(indice), NUMERO_PESSOAS, fdh);
+        INICIO_ARQUIVO(fdin); //coloca no começo do arquivo pessoa
+        //ler cabecalho
+        fread(&h.status, sizeof(char), 1, fdin);
+        fread(&h.quantidadePessoas, sizeof(int), 1, fdin);
+        fread(&h.quantidadeRemovidos, sizeof(int), 1, fdin);
+        INICIO_ARQUIVO(fdin); // coloca no comeco do arquivo pessoa
 
+        indice VetInd[NUMERO_PESSOAS];
+            fseek(fdh, 12, SEEK_SET);
+        //puxando o indice para a RAM
+            for(int in = 0; in < NUMERO_PESSOAS; in++){//copia os indices para o vetor
+                fread(&VetInd[in].idPessoa,sizeof(int),1,fdh);
+                fread(&VetInd[in].Offset,sizeof(long),1,fdh);
+            }
         //busca uma pessoa p com o parametro solicitado
+        for(int k = 0; k < NUMERO_PESSOAS; k++){
         p = busca(fdin, fdh, tipoBusca);
         if(p != NULL){
         //printa a pessoa na tela
@@ -85,16 +93,27 @@ void JUNCAO(char *arquivoEntrada, char *arquivoIndicePrimario, char *arquivoOrde
             fread(&hs.proxRRN, sizeof(int), 1, fdo);
             fread(&hs.quantidadePessoas, sizeof(int), 1, fdo);
 
-            //puxando o segueOrdenado para a RAM
-            segue *segueOrdenado = (segue *)malloc(NUMERO_RRN * sizeof(segue));
-            //lendo o segueOrdenado para a RAM
-            fread(segueOrdenado, sizeof(segue), NUMERO_RRN, fdo);
+            segue VetSegue[hs.quantidadePessoas];
+            fseek(fdo, 9, SEEK_SET);
+        //puxando o indice para a RAM
+            for(int in = 0; in < hs.quantidadePessoas; in++){//copia os indices para o vetor
+                fread(&VetSegue[in].removido,sizeof(char),1,fdo);
+                fread(&VetSegue[in].idPessoaQueSegue,sizeof(int),1,fdo);
+                fread(&VetSegue[in].idPessoaQueESeguida,sizeof(int),1,fdo);
+                VetSegue[in].dataInicioQueSegue = (char *) malloc(11);
+                fread(VetSegue[in].dataInicioQueSegue,sizeof(char),10,fdo);
+                VetSegue[in].dataInicioQueSegue[11]= '\0';
+                VetSegue[in].dataFimQueSegue = (char *) malloc(11);
+                fread(VetSegue[in].dataFimQueSegue,sizeof(char),10,fdo);
+                VetSegue[in].dataFimQueSegue[11]= '\0';
+                fread(&VetSegue[in].grauAmizade, sizeof(char), 1, fdo);
+            }
 
             //realiza a busca binaria no segue ordenado para achar idpessoa == idpessoaQueSegue
-            s = busca_binaria(segueOrdenado, NUMERO_RRN, p->idPessoa);
+            s = busca_binaria(VetSegue, NUMERO_RRN, p->idPessoa);
 
             // percorre todos os registros consecutivos com mesmo idPessoa
-            int idx = (s != NULL) ? (s - segueOrdenado) : -1;
+            int idx = (s != NULL) ? (s - VetSegue) : -1;
             while(p != NULL && (s->idPessoaQueSegue == p->idPessoa)){
                 //idSeguida
                 printf("Segue a pessoa de codigo %d\n", s->idPessoaQueESeguida);
@@ -120,18 +139,17 @@ void JUNCAO(char *arquivoEntrada, char *arquivoIndicePrimario, char *arquivoOrde
                 printf("\n");
                 idx++;
                 // avança para o próximo registro consecutivo
-                if (idx < NUMERO_RRN && segueOrdenado[idx].idPessoaQueSegue == p->idPessoa)
-                    s = &segueOrdenado[idx];
-                else
+                if (idx < NUMERO_RRN && VetSegue[idx].idPessoaQueSegue == p->idPessoa)
+                    s = &VetSegue[idx];
+                if(VetSegue[idx].idPessoaQueSegue != p->idPessoa){
                     break;
+                }
             }
-            // libera memoria
-            free(segueOrdenado);
-            free(p->nomePessoa);
-            free(p->nomeUsuario);
-            free(p);
         }else{
             printf("Registro inexistente\n");
         }
+        }
     }
+    free(p);
 }
+
